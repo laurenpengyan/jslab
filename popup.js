98d0fafe0a7b4263e96e7f5f5bb1ca4a567f4e4a
laurenpengyan <enzopeng1226@gmail.com>
2018-09-29 15:27:40 -0700

    Add initial comments

--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
-window.onload = py_newWinLinks;
+window.onload = py_newWinLinks; // Calls the py_newWinLinks function when the window finishes loading.
 
+/*
+Onload event handler that calls a funciton name py_newWinLinks
+TO cycles through all the links on the page and to see if any of the
+links include a class of py_newWin. If so,when the link is clicked,the
+function calls the py_newWindow funciton
+*/
 function py_newWinLinks() {
 	for (var i=0; i<document.links.length; i++) {
 		if (document.links[i].className == "py_newWin") {
