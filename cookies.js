0f638d0d7b45d21da41e4016b6985950680f129b
laurenpengyan <enzopeng1226@gmail.com>
2018-11-10 16:07:40 -0800

    add cookies count

--- cookies.js
+++ cookies.js
@@ -1,6 +1,8 @@
-window.addEventListener("load", showLastVisit, false);
+window.addEventListener("load", showVisitInfo, false);
 
-function showLastVisit() {
+function showVisitInfo() {
+
+    var visit_count = 0;
 
     // Display last visit cookie if exists
     if (document.cookie != "") {
@@ -9,12 +11,18 @@ function showLastVisit() {
         // Second step split by =
         for (var i = 0; i < thisCookie.length; i++) {
             var key = thisCookie[i].split("=")[0];
+
             if (key == "last_visit") {
                 document.getElementById("last_visit").innerHTML = thisCookie[i].split("=")[1];
+            } else if (key == "visit_count") {
+                visit_count = parseInt(thisCookie[i].split("=")[1]);
+                document.getElementById("visit_count").innerHTML = visit_count;
             }
+
         }
     }
 
-    // Set last visit cookie
+    // Set visit cookies
     document.cookie = "last_visit=" + new Date();
+    document.cookie = "visit_count=" + (++visit_count);
 }
